e604ed090ac88b90c2b9cbda3a870712171ceb33
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-08-26 22:58:41 +0800

    Implement task list

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Luke {
     private static final int HORIZONTAL_LINE_LENGTH = 60;
     private static final String HORIZONTAL_LINE = createHorizontalLine();
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Luke\n" + getLogo());
@@ -16,7 +19,20 @@ public class Luke {
 
         while (!command.equals("bye")) {
             System.out.println(HORIZONTAL_LINE);
-            System.out.println(command);
+
+            if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+
+                System.out.println("added: " + command);
+            }
+
             System.out.println(HORIZONTAL_LINE);
 
             command = input.nextLine();
@@ -29,9 +45,11 @@ public class Luke {
 
     private static String createHorizontalLine() {
         StringBuilder horizontalLine = new StringBuilder();
+
         for (int i = 0; i < HORIZONTAL_LINE_LENGTH; i++) {
             horizontalLine.append("_");
         }
+
         return horizontalLine.toString();
     }
 
