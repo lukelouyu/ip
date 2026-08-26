82ad8b00f160c61f632c1b0771bd83195d66b776
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-08-26 22:12:02 +0800

    Add greeting

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,10 +1,27 @@
 public class Luke {
+    private static final int HORIZONTAL_LINE_LENGTH = 60;
+    private static final String HORIZONTAL_LINE = createHorizontalLine();
+
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Luke\n" + getLogo());
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static String createHorizontalLine() {
+        StringBuilder horizontalLine = new StringBuilder();
+        for (int i = 0; i < HORIZONTAL_LINE_LENGTH; i++) {
+            horizontalLine.append("_");
+        }
+        return horizontalLine.toString();
+    }
+
+    private static String getLogo() {
+        return " _          _        \n"
+                + "| |   _   _| | _____ \n"
+                + "| |  | | | | |/ / _ \\\n"
+                + "| |__| |_| |   <  __/\n"
+                + "|_____\\__,_|_|\\_\\___|\n";
     }
 }
