3886dc748ac39df6304d7f59a916786a599d287a
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-09-02 22:42:46 +0800

    Replace command magic values with constants

--- src/main/java/luke/Luke.java
+++ src/main/java/luke/Luke.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 /**
  * Runs the Luke chatbot's command-line interface.
  */
+
 public class Luke {
-    private static final int MARK_INDEX = 5;
-    private static final int UNMARK_INDEX = 7;
+    private static final String COMMAND_LIST = "list";
+    private static final String COMMAND_TODO = "todo";
+    private static final String COMMAND_DEADLINE = "deadline";
+    private static final String COMMAND_EVENT = "event";
+    private static final String COMMAND_MARK = "mark";
+    private static final String COMMAND_UNMARK = "unmark";
+
+    private static final String COMMAND_SEPARATOR = " ";
+    private static final String DEADLINE_SEPARATOR = " /by ";
+    private static final String EVENT_FROM_SEPARATOR = " /from ";
+    private static final String EVENT_TO_SEPARATOR = " /to ";
+
+    private static final int SPLIT_LIMIT = 2;
     private static final int HORIZONTAL_LINE_LENGTH = 60;
-    private static final String HORIZONTAL_LINE = createHorizontalLine();
     private static final int MAX_TASKS = 100;
+
+    private static final String HORIZONTAL_LINE = createHorizontalLine();
 
     /**
      * Starts Luke and processes commands until the user exits.
@@ -32,23 +45,26 @@ public class Luke {
         while (!command.equals("bye")) {
             System.out.println(HORIZONTAL_LINE);
 
-            String commandWord = command.split(" ")[0];
+            String commandWord = command.split(COMMAND_SEPARATOR, SPLIT_LIMIT)[0];
 
             switch (commandWord) {
-            case "list":
+            case COMMAND_LIST:
                 showTaskList(tasks, taskCount);
                 break;
 
-            case "todo":
-                String todoDescription = command.substring(5);
+            case COMMAND_TODO:
+                String todoDescription = command.substring(
+                        COMMAND_TODO.length() + COMMAND_SEPARATOR.length());
                 tasks[taskCount] = new Todo(todoDescription);
                 taskCount++;
 
                 showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
-            case "deadline":
-                String[] deadlineParts = command.substring(9).split(" /by ", 2);
+            case COMMAND_DEADLINE:
+                String[] deadlineParts = command.substring(
+                                COMMAND_DEADLINE.length() + COMMAND_SEPARATOR.length())
+                        .split(DEADLINE_SEPARATOR, SPLIT_LIMIT);
                 String deadlineDescription = deadlineParts[0];
                 String by = deadlineParts[1];
 
@@ -58,8 +74,10 @@ public class Luke {
                 showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
-            case "event":
-                String[] eventParts = command.substring(6).split(" /from ", 2);
+            case COMMAND_EVENT:
+                String[] eventParts = command.substring(
+                                COMMAND_EVENT.length() + COMMAND_SEPARATOR.length())
+                        .split(EVENT_FROM_SEPARATOR, SPLIT_LIMIT);
                 String eventDescription = eventParts[0];
 
                 String[] timeParts = eventParts[1].split(" /to ", 2);
@@ -72,8 +90,9 @@ public class Luke {
                 showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
-            case "mark":
-                int taskNumber = Integer.parseInt(command.substring(MARK_INDEX));
+            case COMMAND_MARK:
+                int taskNumber = Integer.parseInt(command.substring(
+                        COMMAND_MARK.length() + COMMAND_SEPARATOR.length()));
                 Task task = tasks[taskNumber - 1];
 
                 task.markAsDone();
@@ -82,8 +101,9 @@ public class Luke {
                 System.out.println("  " + task);
                 break;
 
-            case "unmark":
-                int unmarkTaskNumber = Integer.parseInt(command.substring(UNMARK_INDEX));
+            case COMMAND_UNMARK:
+                int unmarkTaskNumber = Integer.parseInt(command.substring(
+                        COMMAND_UNMARK.length() + COMMAND_SEPARATOR.length()));
                 Task unmarkTask = tasks[unmarkTaskNumber - 1];
 
                 unmarkTask.markAsNotDone();
