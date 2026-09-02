c58dfd38f4e34d13471781ca0222010b3fab03b7
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-09-02 23:06:11 +0800

    Extract user interface into Ui

--- src/main/java/luke/Luke.java
+++ src/main/java/luke/Luke.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 /**
  * Runs the Luke chatbot's command-line interface.
  */
-
 public class Luke {
     private static final String COMMAND_LIST = "list";
     private static final String COMMAND_TODO = "todo";
@@ -17,10 +16,7 @@ public class Luke {
     private static final String COMMAND_SEPARATOR = " ";
 
     private static final int SPLIT_LIMIT = 2;
-    private static final int HORIZONTAL_LINE_LENGTH = 60;
     private static final int MAX_TASKS = 100;
-
-    private static final String HORIZONTAL_LINE = createHorizontalLine();
 
     /**
      * Starts Luke and processes commands until the user exits.
@@ -32,61 +28,62 @@ public class Luke {
         Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
-        WelcomeMessage();
+        Ui.showWelcome();
 
         String command = input.nextLine();
 
         while (!command.equals("bye")) {
-            System.out.println(HORIZONTAL_LINE);
+            Ui.showHorizontalLine();
 
-            String commandWord = command.split(COMMAND_SEPARATOR, SPLIT_LIMIT)[0];
+            String commandWord =
+                    command.split(COMMAND_SEPARATOR, SPLIT_LIMIT)[0];
 
             switch (commandWord) {
             case COMMAND_LIST:
-                showTaskList(tasks, taskCount);
+                Ui.showTaskList(tasks, taskCount);
                 break;
 
             case COMMAND_TODO:
                 tasks[taskCount] = Parser.parseTodo(command);
                 taskCount++;
 
-                showTaskAdded(tasks[taskCount - 1], taskCount);
+                Ui.showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
             case COMMAND_DEADLINE:
                 tasks[taskCount] = Parser.parseDeadline(command);
                 taskCount++;
 
-                showTaskAdded(tasks[taskCount - 1], taskCount);
+                Ui.showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
             case COMMAND_EVENT:
                 tasks[taskCount] = Parser.parseEvent(command);
                 taskCount++;
 
-                showTaskAdded(tasks[taskCount - 1], taskCount);
+                Ui.showTaskAdded(tasks[taskCount - 1], taskCount);
                 break;
 
             case COMMAND_MARK:
                 int taskNumber = Integer.parseInt(command.substring(
-                        COMMAND_MARK.length() + COMMAND_SEPARATOR.length()));
-                Task task = tasks[taskNumber - 1];
+                        COMMAND_MARK.length()
+                                + COMMAND_SEPARATOR.length()));
 
+                Task task = tasks[taskNumber - 1];
                 task.markAsDone();
 
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task);
+                Ui.showTaskMarked(task);
                 break;
 
             case COMMAND_UNMARK:
                 int unmarkTaskNumber = Integer.parseInt(command.substring(
-                        COMMAND_UNMARK.length() + COMMAND_SEPARATOR.length()));
-                Task unmarkTask = tasks[unmarkTaskNumber - 1];
+                        COMMAND_UNMARK.length()
+                                + COMMAND_SEPARATOR.length()));
 
+                Task unmarkTask = tasks[unmarkTaskNumber - 1];
                 unmarkTask.markAsNotDone();
 
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + unmarkTask);
+                Ui.showTaskUnmarked(unmarkTask);
                 break;
 
             default:
@@ -97,56 +94,10 @@ public class Luke {
                 break;
             }
 
-            System.out.println(HORIZONTAL_LINE);
+            Ui.showHorizontalLine();
             command = input.nextLine();
         }
 
-        ByeMessage();
-    }
-
-    private static void WelcomeMessage() {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Hello! I'm Luke\n" + getLogo());
-        System.out.println("What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
-    }
-
-    private static void ByeMessage() {
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
-    }
-
-
-    private static void showTaskList(Task[] tasks, int taskCount) {
-        System.out.println("Here are the tasks in your list:");
-
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
-        }
-    }
-
-    private static void showTaskAdded(Task task, int taskCount) {
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + task);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-    }
-
-    private static String createHorizontalLine() {
-        StringBuilder horizontalLine = new StringBuilder();
-
-        for (int i = 0; i < HORIZONTAL_LINE_LENGTH; i++) {
-            horizontalLine.append("_");
-        }
-
-        return horizontalLine.toString();
-    }
-
-    private static String getLogo() {
-        return " _          _        \n"
-                + "| |   _   _| | _____ \n"
-                + "| |  | | | | |/ / _ \\\n"
-                + "| |__| |_| |   <  __/\n"
-                + "|_____\\__,_|_|\\_\\___|\n";
+        Ui.showGoodbye();
     }
 }
---
+++ src/main/java/luke/Ui.java
@@ -0,0 +1,68 @@
+package luke;
+
+/**
+ * Handles the display of messages to the user.
+ */
+public class Ui {
+    private static final int HORIZONTAL_LINE_LENGTH = 60;
+    private static final String HORIZONTAL_LINE = createHorizontalLine();
+
+    public static void showWelcome() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Luke\n" + getLogo());
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void showGoodbye() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void showTaskList(Task[] tasks, int taskCount) {
+        System.out.println("Here are the tasks in your list:");
+
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    public static void showTaskAdded(Task task, int taskCount) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + taskCount + " tasks in the list.");
+    }
+
+    public static void showTaskMarked(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+    }
+
+    public static void showTaskUnmarked(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+    }
+
+    public static void showHorizontalLine() {
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static String createHorizontalLine() {
+        StringBuilder horizontalLine = new StringBuilder();
+
+        for (int i = 0; i < HORIZONTAL_LINE_LENGTH; i++) {
+            horizontalLine.append("_");
+        }
+
+        return horizontalLine.toString();
+    }
+
+    private static String getLogo() {
+        return " _          _        \n"
+                + "| |   _   _| | _____ \n"
+                + "| |  | | | | |/ / _ \\\n"
+                + "| |__| |_| |   <  __/\n"
+                + "|_____\\__,_|_|\\_\\___|\n";
+    }
+}
