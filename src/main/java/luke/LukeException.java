7ce6950b5914618dadb2643de0949582e84e25f7
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-09-06 19:10:52 +0800

    remove ununsed constant

--- src/main/java/luke/LukeException.java
+++ src/main/java/luke/LukeException.java
@@ -4,7 +4,6 @@ package luke;
  * Represents errors caused by invalid user commands.
  */
 public class LukeException extends Exception {
-    private static final long serialVersionUID = 1L;
 
     /**
      * Creates a Luke-specific exception with the given message.
