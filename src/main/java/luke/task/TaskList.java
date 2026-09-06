c81b2be206d06d3c405949b513bf09cea2c9c02d
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-09-06 20:32:39 +0800

    Use ArrayList to store tasks

--- src/main/java/luke/task/TaskList.java
+++ src/main/java/luke/task/TaskList.java
@@ -1,6 +1,6 @@
 package luke.task;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import luke.exception.LukeException;
 
@@ -8,82 +8,47 @@ import luke.exception.LukeException;
  * Stores and manages the user's tasks.
  */
 public class TaskList {
-    private static final int MAX_TASKS = 100;
+    private final ArrayList<Task> tasks;
 
-    private final Task[] tasks;
-    private int taskCount;
 
-    /**
-     * Creates an empty task list.
-     */
     public TaskList() {
-        tasks = new Task[MAX_TASKS];
-        taskCount = 0;
+        tasks = new ArrayList<>();
     }
 
-    /**
-     * Adds a task to the task list.
-     *
-     * @param task Task to add.
-     */
     public void add(Task task) {
-        tasks[taskCount] = task;
-        taskCount++;
+        tasks.add(task);
     }
 
-    /**
-     * Marks the specified task as done.
-     *
-     * @param taskNumber One-based task number.
-     * @return Task that was marked.
-     * @throws LukeException If the task number does not refer to an existing task.
-     */
     public Task mark(int taskNumber) throws LukeException {
         validateTaskNumber(taskNumber);
 
-        Task task = tasks[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markAsDone();
         return task;
     }
 
-    /**
-     * Marks the specified task as not done.
-     *
-     * @param taskNumber One-based task number.
-     * @return Task that was unmarked.
-     * @throws LukeException If the task number does not refer to an existing task.
-     */
     public Task unmark(int taskNumber) throws LukeException {
         validateTaskNumber(taskNumber);
 
-        Task task = tasks[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markAsNotDone();
         return task;
     }
 
     private void validateTaskNumber(int taskNumber) throws LukeException {
-        if (taskNumber < 1 || taskNumber > taskCount) {
+        if (taskNumber < 1 || taskNumber > tasks.size()) {
             throw new LukeException(
                     "That task number does not exist. Please choose the number between 1 and "
-                            + taskCount + ".");
+                            + tasks.size() + ".");
         }
     }
 
-    /**
-     * Returns a copy of all stored tasks.
-     *
-     * @return Array containing the tasks.
-     */
     public Task[] getTasks() {
-        return Arrays.copyOf(tasks, taskCount);
+        return tasks.toArray(new Task[0]);
     }
 
-    /**
-     * Returns the number of stored tasks.
-     *
-     * @return Number of tasks.
-     */
     public int getTaskCount() {
-        return taskCount;
+        return tasks.size();
     }
+
 }
